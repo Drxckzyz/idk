f74fd96ef0e958ef8643daa25e64493e5e9ec422
Drxckzyz <71843072+Drxckzyz@users.noreply.github.com>
2022-02-18 20:23:55 -0400

    Added executeWebhook helper

---
+++ libs/helpers/webhooks/executeWebhook.ts
@@ -0,0 +1,32 @@
+import { Bot } from "../../";
+import { RESTPostAPIWebhookWithTokenJSONBody, Snowflake, APIMessage, Routes } from "discord-api-types/v9"
+
+export async function executeWebhook(bot: Bot, hookId: string, hookToken: string, options: ExecuteWebhookOptions): Promise<APIMessage | null> {
+    const data: RESTPostAPIWebhookWithTokenJSONBody = {
+        allowed_mentions: options.allowed_mentions,
+        attachments: options.attachments,
+        avatar_url: options.avatar_url,
+        components: options.components,
+        content: options.content,
+        embeds: options.embeds,
+        flags: options.flags,
+        tts: options.tts,
+        username: options.username,
+    }
+
+    const res = await bot.rest.post<APIMessage>(Routes.webhook(hookId, hookToken), {
+        body: data,
+        params: {
+            wait: options.wait ?? false,
+            thread_id: options.thread_id ?? "",
+        }
+    })
+
+    if (!options.wait) return null
+    return res
+}
+
+export interface ExecuteWebhookOptions extends RESTPostAPIWebhookWithTokenJSONBody {
+    wait?: boolean;
+    thread_id?: Snowflake;
+}
